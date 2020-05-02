597722bb1d1826b1a8ad8fcc5dfe7f3d3995074f
Yulia1999 <syulianetttogggg@gmail.com>
2020-05-02 17:23:10 +0300

    Edit math_classes

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import telebot
 import math_classes
+import math
 
+BotNAME = 'EngineeringCalculator'
 BotTOKEN = '1196012818:AAFXZQMHfArrsGXQyxfgTAPasDOL15REKGA'
 bot = telebot.TeleBot(BotTOKEN)
-
-
-pi = 3.141592653589793238462643
 
 
 @bot.message_handler(content_types=['start'])
@@ -18,11 +17,18 @@ def help_message(message):
     bot.send_message(message.chat.id, text='Я могу посчитать простое выражение')
 
 
-@bot.message_handler(commands=['calc'])
+@bot.message_handler(commands=['eval'])
 def weather_message(message):
     bot.send_message(message.chat.id, text='Введите запрос в формате:'
                                            '\n4 + 5'
-                                            '\nsin(20)')
+                                            '\nsin(20)'
+                                            '\n 3 ^ (4 / 2)')
+
+
+@bot.message_handler(content_types=["text"])
+def calculator_logic(message):
+    text = message.text
+    bot.send_message(message.chat.id, math_classes.parse(text))
 
 
 if __name__ == '__main__':
